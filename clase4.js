014c3fecd80407f541d310a7b23aa5c2a6a4efd4
Anderson Martinez Duque <anderson.martinezdq@gmail.com>
2020-03-11 23:14:01 -0500

    clase de funciones toma de notas y notas al pie

--- clase4.js
+++ clase4.js
@@ -1,12 +1,14 @@
-var nombre = 'Sacha', edad = 28
+//las funciones son pedazos de código reutilizables bastante útiles en la programaciòn, javascript nos permite crear nuetras propias funciones.
+var nombre = 'Anderson', edad = 19;
 
-function imprimirEdad(n, e) {
-  console.log(`${n} tiene ${e} años`)
+//las inicializamos con el Keyword y el nombre de la función, entre los parentesis podemos declarar los argumentos por medio de variables locales.
+function imprimirEdad(n, e){
+  console.log(`${n} tiene ${e} años`);
 }
 
-imprimirEdad(nombre, edad)
-imprimirEdad('Vicky', 28)
-imprimirEdad('Eric', 24)
-imprimirEdad('Darío', 27)
-imprimirEdad(25, 'Carlos')
-imprimirEdad('Juan')
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 28);
+imprimirEdad('Eric', 24);
+imprimirEdad('Darío', 27);
+imprimirEdad(25, 'Carlos'); //debido a lo debilmente tipado de Js puede suceder que esto se de y no haya un error de sintáxis pero si hay un error lógico, por lo que debemos tener cuidado con nuestras variables.
+imprimirEdad('Juan');//Js por su dinamismo y su estado de lengiaje interpretado, o que corre en ejecuciòn declara la variable pero lo declara como undifined.
