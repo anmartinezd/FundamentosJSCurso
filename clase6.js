5aa422cf6bfc8b439ecd501aa2b2812568927592
Anderson Martinez Duque <anderson.martinezdq@gmail.com>
2020-03-12 13:31:30 -0500

    notas de objetos

--- clase6.js
+++ clase6.js
@@ -3,7 +3,7 @@ var sacha = {
   apellido: 'Lifszyc',
   edad: 28
 }
-
+//un objeto se compone de una clave y valor, es parecido a los diccionarios en python solo que también pueden llevar funciones enre sus valores
 var dario = {
   nombre: 'Darío',
   apellido: 'Susnisky',
@@ -16,5 +16,5 @@ function imprimirNombreEnMayusculas({ nombre }) {
 
 imprimirNombreEnMayusculas(sacha)
 imprimirNombreEnMayusculas(dario)
-// imprimirNombreEnMayusculas({ nombre: 'Pepito' })
-imprimirNombreEnMayusculas({ apellido: 'Gomez' })
+imprimirNombreEnMayusculas({ nombre: 'Pepito' }) //deifiniendo un objeto en los argumentos
+// imprimirNombreEnMayusculas({ apellido: 'Gomez' })
