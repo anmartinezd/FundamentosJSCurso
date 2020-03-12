bd3b7e441ef2277f62b544e76c6e6a38c403b18d
Anderson Martinez Duque <anderson.martinezdq@gmail.com>
2020-03-11 22:34:05 -0500

    notas y desafio primeras 2 clases sobre variables Js

--- clase2.js
+++ clase2.js
@@ -1,11 +1,33 @@
-var nombre = 'Sacha', apellido = 'Lifszyc'
+// var nombre = 'Sacha', apellido = 'Lifszyc'
 
-var nombreEnMayusculas = nombre.toUpperCase()
-var apellidoEnMinusculas = apellido.toLowerCase()
+// var nombreEnMayusculas = nombre.toUpperCase()
+// var apellidoEnMinusculas = apellido.toLowerCase()
 
-var primeraLetraDelNombre = nombre.charAt(0)
-var cantidadDeLetrasDelNombre = nombre.length
+// var primeraLetraDelNombre = nombre.charAt(0)
+// var cantidadDeLetrasDelNombre = nombre.length
 
-var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+// var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
 
-var str = nombre.substr(1, 2)
+// var str = nombre.substr(1, 2)
+
+var nombre = 'Anderson', apellido = 'Martínez';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length; //lenght es un atributo
+
+var nombreCompleto = nombre + ' ' + apellido;
+
+//interpolación de texto
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase}` // dentro de las llaves se puede incluir código Javascript permitiendo por ejemplo utilizar un variable.toUpperCase() con total normalidad
+
+var str = nombre.substr(1, 2);
+
+//desafio
+
+nombre = 'Juan';
+
+var ultimaLetra = `${nombre.charAt(nombre.length - 1)}`;
